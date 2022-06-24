d6a9df5670567d26ebdb6fff9726f92cfabb7e4e
TNT <86717030+GGTNT@users.noreply.github.com>
2022-06-24 11:17:37 +0200

    ajout de timer

---
+++ timer.js
@@ -0,0 +1,21 @@
+const text = document.querySelector('h2')
+
+function getChrono(){
+    const now = new Date().getTime();
+    const countdownDate = new Date('Jun 30, 2022').getTime();
+    const distanceBase = countdownDate - now;
+    const days = Math.floor(distanceBase / (1000*60*60*24));
+    const hours = Math.floor((distanceBase % (1000*60*60*24)) / (1000*60*60*24))
+
+    const minutes = Math.floor((distanceBase % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((distanceBase % (1000 * 60)) / 1000 )
+
+    text.innerText = `${days}j ${hours}h ${minutes}m ${seconds}s`;
+
+}
+
+getChrono()
+
+const countdownInterval = setInterval(() => {
+    getChrono()
+}, 1000);
